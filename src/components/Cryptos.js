a10ee89493da7c4bc0df930ffe4f31487a8d0c59
Greimer29 <greimerabelguzman29@gmail.com>
2022-01-24 08:52:43 -0400

    Agregando Link a los cryptos

--- src/components/Cryptos.js
+++ src/components/Cryptos.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import {Link} from "react-router-dom";
 import "./css/cryptos.scss";
 
 class Cryptos extends Component {
   render() {
     return (
       <div className="cryptos rounded">
-        <div className="ms-2"># {this.props.rank}</div>
-        <div className="cryptos_logo ms-3 me-3">Logo</div>
+        <Link to="/cryptocurrencies" className="ms-2"># {this.props.rank}</Link>
+        <div className="cryptos_logo ms-3 me-3"><img src={this.props.logo} alt="logo"/></div>
         <div className="cryptos_name ms-3 me-3">
           <div>{this.props.nombre}</div>
           <div>{this.props.apellido}</div>
